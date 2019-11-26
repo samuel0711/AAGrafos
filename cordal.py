28d9dc5d58edd17ae9529c8f6da6efd7850965f9
Samuel Pessoa <samuel.santos1333@gmail.com>
2019-11-25 23:06:15 -0300

    Add files via upload
    Correção da identificação das arestas no ciclo.
    Criação de 2 listas de arestas ((u,v) e (v,u)) para auxiliar na detecção de arestas fora do ciclo.
    Correção da condição para a aresta ser uma corda.
    Isso tudo pra descobrir que a função networkx.simple_cycles e cycle_basis não identificavam certos ciclos ¯\_(ツ)_/¯

--- cordal.py
+++ cordal.py
@@ -1,25 +1,64 @@
-from collections import defaultdict
+import psutil
 import networkx as nx
 
 grafos = nx.DiGraph()
+#grafos.add_nodes_from(['A','B','C','D'])
 grafos.add_nodes_from(['A','B','C','D','E'])
-grafos.add_edges_from([('A','B'), ('B','C'), ('C','D'),('D','E'),('E','A')])
-#edges = [('A','B'), ('B','C'), ('C','D'),('D','E'),('E','A'), ('A','D')]
+#grafos.add_nodes_from(['A','B','C','D','E','F'])
+#grafos.add_edges_from([('A','B'), ('A','C'), ('B','C'), ('C','D'), ('B','D')])
+#grafos.add_edges_from([('A','B'), ('B','C'), ('C','D'),('D','E'),('E','A')])
+#grafos.add_edges_from([('A','B'), ('B','C'), ('C','D'),('D','E'),('E','A'), ('A','D')])
+grafos.add_edges_from([('A','B'), ('A','C'), ('B','C'), ('C','E'),('C','D'), ('E','D')])
+
+
+print(list(nx.find_cycle(grafos, orientation='ignore')))
 
 ciclos = list(nx.simple_cycles(grafos))
 ciclos.sort(key=len)
 cordaisCiclo = False
+arestas = []
+edges = []
+print("Existem ",len(ciclos)," ciclos.")
 for ciclo in ciclos:
+	print("CICLO: ",ciclo)
+	for edge in range(len(ciclo)):
+		if edge == 0:
+			primeira = ciclo[0]
+			print("primeira aresta: ",primeira)
+			x = ciclo[edge]
+			continue
+		elif len(ciclo) > 3:
+			arestas.append((x,ciclo[edge]))
+			edges.append((ciclo[edge],x))
+			x = ciclo[edge]
+	if len(ciclo) >3:
+		arestas.append((primeira,x))
+		edges.append((x,primeira))
+
+	print(arestas)
+	print(edges)
+	cordaisCiclo = False
 	if len(ciclo) == 3:
 		print("O ciclo ",ciclo,"tem tamanho 3, logo é um ciclo induzido. Não define cordalidade ao grafo.")
 	else:
 		for vertice in ciclo:
 			for v in grafos.nodes:
-				if(vertice != v and vertice not in grafos.neighbors(v) and v not in grafos.neighbors(vertice) and (grafos.has_edge(vertice,v) or grafos.has_edge(v,vertice))):
-					print("A aresta (",vertice,",",v,") não faz parte do ciclo, porém conecta dois vértices do ciclo ",ciclo,".")
-					cordaisCiclo = True
+				aux = (vertice,v)
+				print(vertice,v)
+				if(vertice != v and (v in ciclo and (aux not in arestas and aux not in edges) ) ):
+					print("Achou a confição: ",vertice,v)
+					if(grafos.has_edge(vertice,v) or grafos.has_edge(v,vertice)):
+						print("A aresta (",vertice,",",v,") não faz parte do ciclo, porém conecta dois vértices do ciclo ",ciclo,".")
+						cordaisCiclo = True
 					break
-			break
+			if(cordaisCiclo == True):
+				break
 		if cordaisCiclo == False:
 			print("O ciclo ",ciclo," não possui uma corda. Ou seja, o grafo não é cordal!")
-			break
+			print("Uso da CPU: ",psutil.cpu_percent(),"%")
+			exit()
+if cordaisCiclo != False:
+	print("O grafo é cordal!")
+else:
+	print("O grafo não é cordal!")
+print("Uso da CPU: ",psutil.cpu_percent(),"%")
